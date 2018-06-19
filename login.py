13677f2b72a086d774f6626832bfdf7e040e307c
limyheart <itcart@it.com>
2018-06-19 10:53:27 +0800

    增加了一个Index类

--- login.py
+++ login.py
@@ -3,3 +3,13 @@ print("hello world")
 def demo():
 	return "ok!"
 demo()
+
+class Index(object):
+	def __init__(self, name):
+		self.name = name
+
+	def demo_print(self):
+		print(self.name)
+
+tom = Index("xiaoming")
+tom.demo_print()
